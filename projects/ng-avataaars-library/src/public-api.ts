b148df1a756c4758e5f41923779de65b9ca98ff1
KIPMCGRATH\peter.messenger <peter.messenger@kipmcgrath.com.au>
2020-02-09 17:08:09 +1100

    Fix build process for angular 9

--- projects/ng-avataaars-library/src/public-api.ts
+++ projects/ng-avataaars-library/src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of ng-avataaars-library
  */
 
-export * from './lib/custom-avatar/custom-avatar.component';
+export { CustomAvatarComponent }  from './lib/custom-avatar/custom-avatar.component';
+export { CustomAvatarBaseComponent } from './lib/custom-avatar/avatar/avatar.component';
 export * from './lib/ng-avataaars-library.module';
